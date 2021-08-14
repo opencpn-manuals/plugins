8cc623048ec622dd5542849a008ba714c9220618
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-14 14:38:44 +0200

    full linkcheck: Update excluded stuff, docs.

--- ci/w3c-linkcheck.sh
+++ ci/w3c-linkcheck.sh
@@ -20,10 +20,15 @@ checklink \
     --suppress-broken '403:$dokuwiki?id=opencpn:opencpn_user_manual' \
     --suppress-broken '-1:https://cubian.org/downloads/' \
     --suppress-broken '-1:https://cubian.org/2013/08/12/enlarge-cubian-rootfs-partition/' \
-    --suppress-redirect 'https://opencpn-manuals.github.io/development->https://opencpn-manuals.github.io/development/' \
-    --exclude cruisersforum.com \
-    --exclude bigdumboat.com \
-    --exclude 'https://opencpn.org/wiki/dokuwiki/' \
-    --exclude opencpn.org \
+    --suppress-redirect 'https://opencpn-manuals.github.io/plugins->https://opencpn-manuals.github.io/plugins/' \
+    --exclude adsabs.harvard.edu \
     --exclude apple.com \
+    --exclude bethandevans.com \
+    --exclude bigdumboat.com \
+    --exclude cruisersforum.com \
+    --exclude docs.google.com \
+    --exclude opencpn.org/wiki/dokuwiki \
+    --exclude opencpn.org \
+    --exclude youtube.com \
+    --exclude youtu.be \
     --depth 4 -s docs/index.html |& grep -v "Use of uninitialized value"
